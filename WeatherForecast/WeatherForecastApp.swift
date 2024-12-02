9e38210ac0b6570c7d6f91c7a22b2821499b2695
purnami <purnami.indryaswari@gmail.com>
2024-12-03 07:02:01 +0800

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by purnami indryaswari on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
